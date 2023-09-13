1f365d22e1aecfe096231c3b4afaea21c3e4893d
Paul Theby <paultheby@gmail.com>
2023-09-12 21:50:24 -0500

    Watched Observer Pattern Video
    Coded Along while watching. have very basic understanding and looking into other resouces because there are many things i have not learned yet being discussed. like all the document queries and actions on

--- main.js
+++ main.js
@@ -43,13 +43,86 @@
 
 
 
+//not coding along it's very garbled and makes no sense anyways
+
+//4 Observable Design Patterns
+
+// class Observable {
+//     constructor() {
+//         this.subscribers= []
+//     }
+//     subscriber(fn) {
+//         this.subscribers.push(fn);
+//     }
+//     unsubsribe(fn) {
+//         this.subscribers =this.subscribers.filter((item) => item !== fn);
+//     }
+// broadcast(data) {
+//     for( let i=0; i<this.subscribers.length; i++) {
+//         this.subscribers[i](data);
+//     }
+// }
+// }
+
+// const observer= new Observable()
+
+// const fn= (data) => {
+//     console.log('Callback was executed with data', data)
+// };
+// observer.subscriber(fn);
+
+// observer.broadcast('Hello from the observable!');
+
+//can also do this as a function
 
 
 
+function createObservable() {
+    return  {
+        subscribers: [],
+    
+        
+//         subscribe(fn) {
+//             this.subscribers.push(fn);
+//         },
+        
+//         unsubsribe(fn) {
+//             this.subscribers =this.subscribers.filter((item) => item !== fn);
+//         },
+        
+//         broadcast(data) {
+//         for( let i=0; i<this.subscribers.length; i++) {
+//             this.subscribers[i](data);
+//         }
+//     },
+//     };
 
+// }
+// function callbackFn(count) {
+//     document.getElementById('ocount').innerText=count;
+// }
+       
+        
+//  obersever.subscribe(callbackFn);  
 
-
-
+//  document.getElementById('inc').addEventListener('click', function()){
+//     observer.broadcast(
+//         parseInt(document.getElementById('count').innerText, 10) +1
+//     );
+//  };
+    
+    
+ 
+ 
+ 
+//  const observer= createObservable();
+    
+//     const fn= (data) => {
+//         console.log('Callback was executed with data', data)
+//     };
+//     observer.subscribe(fn);
+    
+//     observer.broadcast('Hello from the observable!');
 
 
 
